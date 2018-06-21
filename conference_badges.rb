452626fb18572be37e33cce8e83b5db4df1dd9bd
William Chabla <w_chabla@hotmail.com>
2018-06-21 20:27:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,13 @@
 
 def badge_maker(name)
   puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  array_badges = []
+  array.each do |name|
+    array_badges << badge_maker(name)
+  end
+end
+
